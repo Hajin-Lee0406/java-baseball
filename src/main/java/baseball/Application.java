4f24ae0e4b104541251c16a05537fb6f656d41a8
Hajin-Lee0406 <glgkwls1@naver.com>
2023-10-02 17:02:29 +0900

    리팩토링 - depth가 3을 넘지 않는 조건 해결

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,26 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.*;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args){
 
-        // 난수 생성하기
-        String ranNum = getRandom();
+        boolean valid = true;
+        while (valid){
+            // 난수 생성하기
+            String ranNum = getRandom();
 
-        System.out.println(ranNum);
-        System.out.println("숫자 야구 게임을 시작합니다");
+            System.out.println(ranNum);
+            System.out.println("숫자 야구 게임을 시작합니다");
 
-        while (true) {
-            boolean result = game(ranNum);
-            if(result){
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-                Scanner scan = new Scanner(System.in);
-                int str = scan.nextInt();
+            while (!game(ranNum));
 
-                if (str == 2) break;
-            }
+            valid = continueGame();
         }
     }
 
@@ -29,10 +26,9 @@ public class Application {
         List<String> num = new ArrayList<>();
         for (int i=0; i<10; i++) num.add(String.valueOf(i));
 
-        Random random = new Random();
         StringBuilder ranNum = new StringBuilder();
         for (int i=0; i<3; i++){
-            String a = num.get(random.nextInt(10));
+            String a = num.get(Randoms.pickNumberInRange(1, 9));
             ranNum.append(a);
             num.remove(a);
         }
@@ -60,7 +56,7 @@ public class Application {
         return ball + "볼" + " " + strike + "스트라이크";
     }
 
-    // 야구 게임
+    // 숫자 맞추기
     private static boolean game(String ranNum){
         System.out.println("숫자를 입력해주세요 :");
         Scanner scan = new Scanner(System.in);
@@ -71,4 +67,19 @@ public class Application {
 
         return validation.equals("3스트라이크");
     }
+
+    // 게임 끝내기
+    private static boolean continueGame(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+
+        Scanner scan = new Scanner(System.in);
+        int str = scan.nextInt();
+
+        if (str != 1 && str !=2){
+            throw new IllegalArgumentException("입력값이 올바르지 않습니다.");
+        }
+
+        return str == 1;
+    }
+
 }
