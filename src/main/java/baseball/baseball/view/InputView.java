a48a17daa9f576c8672db76e780d4bc156c14aad
Hajin-Lee0406 <glgkwls1@naver.com>
2023-10-17 17:10:21 +0900

    ref: view class 작성

--- src/main/java/baseball/baseball/view/InputView.java
+++ src/main/java/baseball/baseball/view/InputView.java
@@ -1,5 +1,7 @@
 package baseball.baseball.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
     private static final InputView instance = new InputView();
 
@@ -8,5 +10,11 @@ public class InputView {
     }
 
     private InputView() {
+    }
+
+    public String getNumber(){
+        System.out.print("숫자를 입력해주세요 : ");
+
+        return readLine();
     }
 }
